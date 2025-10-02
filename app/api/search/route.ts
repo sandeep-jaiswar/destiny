338fd6128f4325c6af53f56f7b6b2a347fa0cda9
Sandeep Jaiswar <jaiswarsandeep119@gmail.com>
2025-10-02 18:04:51 +0530

    feat: implement GET endpoint for Yahoo Finance search

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,9 +1,7 @@
-// app/api/users/route.ts
-
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import yahooFinance from 'yahoo-finance2';
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   const results = await yahooFinance.search('INFY.NS');
 
   return NextResponse.json(results);
