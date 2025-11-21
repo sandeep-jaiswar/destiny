650849e9e6721b5b12bd7c7d6d5038894b79e633
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2025-11-21 22:35:38 +0530

    feat: update Next.js configuration with compression and strict mode settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compress: true,
+  reactStrictMode: true,
+  cleanDistDir: true,
 };
 
 export default nextConfig;
